17c62abbbfe35782c342dce024cd64ad485227fb
eleonora <eleonoramonni04@gmail.com>
2026-04-22 00:51:25 +0200

    attributi e metodi di classe

--- lezione/attributi_e_metodi_di_classe.py
+++ lezione/attributi_e_metodi_di_classe.py
@@ -1,12 +1,30 @@
-class CProgrammatori:
-    _stipendio_base = 1500
+class CProgrammatore:
+    stipendio_base = 1500
+
     def __init__(self, nome, cognome):
         self.nome = nome
         self.cognome = cognome
 
 
-class CProgrammatoriJunior(CProgrammatori):
+class CProgrammatoreJunior(CProgrammatore):
+    @classmethod
+    def calcola_stipendio(cls, mese):
+        if mese == 12:
+            return cls.stipendio_base + 500
+        else:
+            return cls.stipendio_base
 
 
+class CProgrammatoreSenior(CProgrammatore):
+    @classmethod
+    def calcola_stipendio(cls, mese):
+        if mese == 12:
+            return cls.stipendio_base + 1000
+        else:
+            return cls.stipendio_base
 
-class CProgrammatoriSenior(CProgrammatori):
+
+if __name__ == "__main__":
+    print("Lo stipendio di un programmatore junior a dicembre è: ", CProgrammatoreJunior.calcola_stipendio(12))
+    print("Lo stipendio di un programmatore senior a dicembre è: ", CProgrammatoreSenior.calcola_stipendio(12))
+    print("Lo stipendio di un programmatore junior a luglio è: ", CProgrammatoreJunior.calcola_stipendio(7))
